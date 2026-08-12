957856ee6f0a61f5cdb952cfd5f849a931423031
Luis Suárez <luis.suarez.menchu@gmail.com>
2026-08-12 11:52:59 -0600

    actualizacion
    de los otros compañeros

--- estudiantes/Christian Ramirez - 202308041.md
+++
@@ -1,2 +0,0 @@
-Me gusta silksong
-Me gusta la lluvia
---
+++ supabase/migrations/202308041-christian-ramirez.sql
@@ -0,0 +1,9 @@
+CREATE TABLE christiantable (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    dato TEXT
+);
+
+
+INSERT INTO christiantable (nombre, dato) 
+VALUES ('Christian Ramirez', 'saquen ya el dlc de silkson');
