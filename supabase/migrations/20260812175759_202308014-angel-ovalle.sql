1e80624e56843f9179948f43f66801a7df609d33
Angel <angelitoovalle10barsa@gmail.com>
2026-08-12 12:18:12 -0600

    Ajuste de migracion: insertar usuario Angel Ovalle 202308014

--- supabase/migrations/20260812175759_202308014-angel-ovalle.sql
+++ supabase/migrations/20260812175759_202308014-angel-ovalle.sql
@@ -1,10 +1,5 @@
 -- Migracion parcial: insertar usuario propio
 -- Angel Ovalle - 202308014
 
-INSERT INTO usuarios (id_usuario, email, password, rol)
-VALUES (
-    gen_random_uuid(),
-    'angel.ovalle.202308014@ejemplo.com',
-    'parcial2026',
-    'Vendedor'
-);
+INSERT INTO usuarios (email, password, rol)
+VALUES ('angel.ovalle.202308014@ejemplo.com', 'parcial2026', 'Vendedor');
